ac42c551751d7d5b26b37840c54e4ef6d0909029
Kalu Singh Rao <kalusinghrao07@gmail.com>
2022-05-03 20:07:52 +0530

    Write on Disk functionality
    In this commit, i have added how to write and read blockchain details on disk to mine the bitcoins.

--- Blockchain/Backend/core/blockchain.py
+++ Blockchain/Backend/core/blockchain.py
@@ -4,16 +4,23 @@ sys.path.append("/Bitcoin")
 from Blockchain.Backend.core.block import Block
 from Blockchain.Backend.core.blockheader import BlockHeader
 from Blockchain.Backend.utils.util import hash256
+from Blockchain.Backend.core.database.database import BlockchainDB
 import time
-import json
 
 ZERO_HASH = '0' * 64
 VERSION = 1
 
 class Blockchain:
     def __init__(self):
-        self.chain = []
         self.GenesisBlock()
+
+    def write_on_disk(self, block):
+        blockchainDB = BlockchainDB()
+        blockchainDB.write(block)
+
+    def fetch_last_block(self):
+        blockchainDB = BlockchainDB()
+        return blockchainDB.lastBlock()
     
     def GenesisBlock(self):
         BlockHeight = 0
@@ -25,16 +32,15 @@ class Blockchain:
         Transaction = f"Singh sent {BlockHeight} Bitcoins to Joshi"
         merkleRoot = hash256(Transaction.encode()).hex()
         bits = "ffff001f"
-        blockheader = BlockHeader(VERSION,prevBlockHash, merkleRoot, timestamp, bits)
+        blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
         blockheader.mine()
-        self.chain.append(Block(BlockHeight, 1,blockheader.__dict__, 1, Transaction).__dict__)
-        print(json.dumps(self.chain, indent=4))
+        self.write_on_disk([Block(BlockHeight, 1,blockheader.__dict__, 1, Transaction).__dict__])
 
     def main(self):
         while True:
-            lastBlock = self.chain[::1]
-            BlockHeight = lastBlock[0]["Height"] + 1
-            prevBlockHash = lastBlock[0]['BlockHeader']['prevBlockHash']
+            lastBlock = self.fetch_last_block()
+            BlockHeight = lastBlock["Height"] + 1
+            prevBlockHash = lastBlock['BlockHeader']['blockHash']
             self.addBlock(BlockHeight, prevBlockHash)
 
 
