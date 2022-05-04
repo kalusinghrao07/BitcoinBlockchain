2d571699cbf4597d2dad170ffb6a0d22c5537dff
Kalu Singh Rao <kalusinghrao07@gmail.com>
2022-05-04 23:06:02 +0530

    Create classes for Transaction
    In this Commit, i am creating required classes for transaction.

---
+++ Blockchain/Backend/core/Tx.py
@@ -0,0 +1,26 @@
+from Blockchain.Backend.core.Script import Script
+
+class Tx:
+    def __init__(self, version, tx_ins, tx_outs, locktime):
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.locktime = locktime
+
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig = None, sequence = 0xffffffff):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+
+        if script_sig is None:
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
+
+        self.sequence = sequence
+
+class TxOut:
+    def __init__(self, amount, script_pubkey):
+        self.amount = amount
+        self.script_pubkey = script_pubkey
+
